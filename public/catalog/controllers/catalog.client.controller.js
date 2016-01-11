7bf60148449925bb95b5abbb82afc424d5d95389
David Chang <dchang@nascentdigital.com>
2016-01-11 00:47:42 -0500

    Implement sorting function (title/year/duration)

--- public/catalog/controllers/catalog.client.controller.js
+++ public/catalog/controllers/catalog.client.controller.js
@@ -1,17 +1,14 @@
 angular.module('catalog').controller('CatalogController', ['$scope', '$http',
-  function($scope, $http) {
+  function ($scope, $http) {
     $scope.name = 'Shomi Catalog';
     $scope.data = null;
+    $scope.sortBy = 'year';
 
     // Simple GET request example:
     $http({
       method: 'GET',
       url: 'http://davidchchang.com/shomi/fetch_feed.php'
     }).then(function successCallback(response) {
-      // TODO: remove logs
-      console.log('success');
-      console.log(response);
-
       // render list to page
       $scope.data = response.data;
 
@@ -23,5 +20,26 @@ angular.module('catalog').controller('CatalogController', ['$scope', '$http',
       // called asynchronously if an error occurs
       // or server returns response with an error status.
     });
+
+    $scope.sortByYear = function () {
+      $scope.sortBy = 'year';
+    };
+    $scope.sortByTitle = function () {
+      $scope.sortBy = 'title';
+    };
+    $scope.sortByDuration = function () {
+      $scope.sortBy = 'duration';
+    };
+
+    $scope.sortingFunction = function (card) {
+      if ($scope.sortBy == 'year') {
+        return card.Item.ReleaseYear == null ? "" : card.Item.ReleaseYear;
+      } else if ($scope.sortBy == 'duration') {
+        return card.Item.RunTimeSec == null ? 0 : card.Item.RunTimeSec;
+      } else {
+        // assume sort by title
+        return card.Item.Title;
+      }
+    };
   }
 ]);
