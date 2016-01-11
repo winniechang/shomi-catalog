9a4b111c94251c92f572d6c217f6ee725a11711b
David Chang <dchang@nascentdigital.com>
2016-01-11 09:30:15 -0500

    Implement pagination controls using built-in limitTo filter

--- public/catalog/controllers/catalog.client.controller.js
+++ public/catalog/controllers/catalog.client.controller.js
@@ -1,12 +1,14 @@
 angular.module('catalog').controller('CatalogController', ['$scope', '$http',
   function ($scope, $http) {
     $scope.name = 'Shomi Catalog';
-    $scope.data = null;
+    $scope.data = {
+      Data: []
+    };
     $scope.sortBy = 'title';
     $scope.paginationObject = {
       currentIndex: 0,
       totalObjects: 0,
-      totalPages: 2
+      totalPages: 1
     };
 
     // Simple GET request example:
@@ -16,6 +18,7 @@ angular.module('catalog').controller('CatalogController', ['$scope', '$http',
     }).then(function successCallback(response) {
       // render list to page
       $scope.data = response.data;
+      $scope.paginationObject.totalPages = Math.ceil($scope.data.TotalHits / 20);
 
       // this callback will be called asynchronously
       // when the response is available
@@ -25,6 +28,8 @@ angular.module('catalog').controller('CatalogController', ['$scope', '$http',
       // called asynchronously if an error occurs
       // or server returns response with an error status.
     });
+
+    // SORTING
 
     $scope.sortByYear = function () {
       $scope.sortBy = 'year';
@@ -46,5 +51,40 @@ angular.module('catalog').controller('CatalogController', ['$scope', '$http',
         return card.Item.Title;
       }
     };
+
+    // PAGINATION
+
+    $scope.gotoPage = function (index) {
+      $scope.paginationObject.currentIndex = index - 1;
+      console.log($scope.paginationObject.currentIndex);
+    };
+    $scope.previousPage = function () {
+      $scope.paginationObject.currentIndex--;
+      if ($scope.paginationObject.currentIndex < 0) {
+        $scope.paginationObject.currentIndex = 0;
+      }
+      console.log($scope.paginationObject.currentIndex);
+    };
+    $scope.nextPage = function () {
+      $scope.paginationObject.currentIndex++;
+      if ($scope.paginationObject.currentIndex >= $scope.paginationObject.totalPages) {
+        $scope.paginationObject.currentIndex = $scope.paginationObject.totalPages - 1;
+      }
+      console.log($scope.paginationObject.currentIndex);
+    };
+
+    // HELPER FUNCTIONS
+
+    // range function from http://stackoverflow.com/a/17124017/24731
+    $scope.range = function (min, max, step) {
+      step = step || 1;
+      var input = [];
+      for (var i = min; i <= max; i += step) {
+        input.push(i);
+      }
+      return input;
+    };
+
+
   }
 ]);
