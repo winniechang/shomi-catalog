eb9f923e632d9b41374d8cfd32bf3350d5b53730
David Chang <dchang@nascentdigital.com>
2016-01-10 22:32:56 -0500

    Setup basic controller, service and view for catalog.

---
+++ public/catalog/controllers/catalog.client.controller.js
@@ -0,0 +1,5 @@
+angular.module('catalog').controller('CatalogController', ['$scope', 'CatalogService',
+  function($scope, CatalogService) {
+    $scope.name = 'Shomi Catalog';
+  }
+]);
---
+++ public/catalog/services/catalog.client.service.js
@@ -0,0 +1,12 @@
+angular.module('catalog').service('CatalogService', [
+  function() {
+    this.someValue = true;
+
+    this.firstMethod = function() {
+
+    };
+    this.secondMethod = function() {
+
+    };
+  }
+]);
